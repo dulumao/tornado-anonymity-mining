2077b996c8edef296d1be8cca92514eb6de1436e
poma <semenov.roma@gmail.com>
2021-01-28 18:15:38 +0300

    wip

--- circuits/input.js
+++ circuits/input.js
@@ -1,16 +1,58 @@
 const fs = require('fs')
-const jsSHA = require("jssha")
-const { BN, toBN } = require('web3-utils')
+const jsSHA = require('jssha')
+const { toBN } = require('web3-utils')
+const MerkleTree = require('fixed-merkle-tree')
+const { poseidonHash2, poseidonHash } = require('../src/utils')
+const Controller = require('../src/controller')
+const contoller = new Controller()
 
-const input = "0x13f47d13ee2fe6c845b2ee141af81de858df4ec549a58b7970bb96645bc8d2"
-const bytes = toBN(input).toBuffer('be', 31)
-console.log('input:     ', bytes.toString('hex'))
+function hashInputs(input) {
+  const sha = new jsSHA('SHA-256', 'ARRAYBUFFER')
+  sha.update(toBN(input.oldRoot).toBuffer('be', 32))
+  sha.update(toBN(input.newRoot).toBuffer('be', 32))
+  sha.update(toBN(input.pathIndices).toBuffer('be', 1))
 
-const shaObj = new jsSHA("SHA-256", "ARRAYBUFFER", {  })
-shaObj.update(bytes)
-const hash = shaObj.getHash("HEX")
-console.log('js hash:   ', hash)
+  for (let i = 0; i < input.instances.length; i++) {
+    sha.update(toBN(input.instances[i]).toBuffer('be', 20))
+    sha.update(toBN(input.hashes[i]).toBuffer('be', 32))
+    sha.update(toBN(input.blocks[i]).toBuffer('be', 4))
+  }
 
-const shaSnark = require('./witness.json')[1]
-const snarkHash = toBN(shaSnark).toBuffer().toString('hex')
-console.log('snark hash:  ', snarkHash)
+  const hash = sha.getHash('HEX')
+  console.log('raw hash', hash)
+  const result = toBN(hash).mod(toBN('21888242871839275222246405745257275088548364400416034343698204186575808495617')).toString('hex')
+  return result
+}
+
+const tree = new MerkleTree(20, [], { hashFunction: poseidonHash2 })
+const leaves = [
+  {
+    instance: '0x3535249DFBb73e21c2aCDC6e42796d920A0379b7',
+    hash: '0x8d5d6393dda443b4f2ad47562908899c5ff8cfe271ac4c55f1e2cafcb58f97',
+    block: 1
+  },
+  {
+    instance: '0x3535249DFBb73e21c2aCDC6e42796d920A0379b7',
+    hash: '0x8d5d6393dda443b4f2ad47562908899c5ff8cfe271ac4c55f1e2cafcb58f97',
+    block: 2
+  },
+  {
+    instance: '0x3535249DFBb73e21c2aCDC6e42796d920A0379b8',
+    hash: '0x8d5d6393dda443b4f2ad47562908899c5ff8cfe271ac4c55f1e2cafcb58f98',
+    block: 3
+  },
+  {
+    instance: '0x3535249DFBb73e21c2aCDC6e42796d920A0379b9',
+    hash: '0x8d5d6393dda443b4f2ad47562908899c5ff8cfe271ac4c55f1e2cafcb58f98',
+    block: 4
+  },
+]
+
+const input = contoller.batchTreeUpdate(tree, leaves)
+console.log(input)
+
+const hash = hashInputs(input)
+console.log('hash: ', hash)
+//delete input.pathElements
+input.argsHash = '0x' + hash
+fs.writeFileSync('input.json', JSON.stringify(input, null, 2))
