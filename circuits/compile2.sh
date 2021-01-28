d7f71fb0ce6c353a25dc516e5a4f8fa19b3196a1
poma <semenov.roma@gmail.com>
2021-01-28 18:13:46 +0300

    wip

--- circuits/compile2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-npx circom -rwsv Utils.circom
-zkutil setup -c Utils.r1cs -p Utils.params
-zkutil generate-verifier -p Utils.params -v ../build/circuits/UtilsVerifier.sol
--- circuits/prove2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-## put non-hashed inputs into input_raw.json
-node input_prod.js
-npx snarkjs wd Utils.wasm
-npx snarkjs wej
-zkutil prove -c Utils.r1cs -p Utils.params
-zkutil verify -p Utils.params
